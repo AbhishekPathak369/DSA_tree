04bf1b249af023577834bd52450386c12114066d
AbhishekPathak369 <pathakabhi290@gmail.com>
2025-08-27 19:17:40 +0530

    max

--- max_node4.java
+++ max_node4.java
@@ -1,4 +1,6 @@
-public class  max_node{
+
+
+public class  max_node4{
     // package trees;
   static int size =0;
 
@@ -17,35 +19,33 @@ public class  max_node{
     }
 
   public static int max(Node root){
-    if(root == null) return 0;
+    // if(root == null) return 0; // this give error in case o value is -ve
+   if(root == null) return Integer.MIN_VALUE;
     int a = root.val;
     int b = max(root.left);
     int c = max(root.right);
-    return Math.max(a,Math.max(b, c));
+    return Math.max(a,Math.max(b, c)); 
+    // return Math.max(root.val,Math.max(max(root.left),max(root.right))); // we also minimize it 
   }
 
     public static void main(String[] args) {
-        Node root = new Node(1);
+        Node root = new Node(-1);
        
         // System.out.println(root.val);
-        Node a = new Node(2);
+        Node a = new Node(-2);
         // System.out.println(a.val);
-        Node b = new Node(3);
+        Node b = new Node(-3);
         root.left = a;
         root.right = b;
-                Node c = new Node(4);
-        Node d = new Node(50);
+        Node c = new Node(-4);
+        Node d = new Node(-50);
         a.left = c;
         a.right = d;
-        Node e = new Node(6);
+        Node e = new Node(-6);
         b.right = e;
 
         System.out.println(max(root));
-   
-
-
-
-       
+  
     }
 
 
