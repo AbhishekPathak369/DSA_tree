478fcf91318b13926563065910d4101b5af0c6fc
AbhishekPathak369 <pathakabhi290@gmail.com>
2025-08-28 19:45:33 +0530

    traversel

---
+++ travesel/inorder.java
@@ -0,0 +1,44 @@
+public class inorder {
+    
+    public static class Node {
+        int val; // initial at 0
+        Node left; // initially at null 
+        Node right; // ''''''
+
+        // also create constructor
+        
+        public Node(int val){
+            this.val = val;
+        }
+
+    }
+
+public static void inorder(Node root){
+    // inorder is  left root right
+    if(root ==  null) return;
+    inorder(root.left);
+    System.out.println(root.val);
+    inorder(root.right);
+}
+
+    public static void main(String[] args) {
+       
+    Node root = new Node(1);
+       
+        Node a = new Node(2);
+        Node b = new Node(3);
+        root.left = a;
+        root.right = b;
+        Node c = new Node(4);
+        Node d = new Node(5);
+   a.left = c;
+   a.right = d;
+   Node e = new Node(6);
+   Node f = new Node(7);
+   b.left = e;
+   b.right= f;
+   
+     inorder(root);
+  
+    }
+}
